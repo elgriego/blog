d066ca2a56c6e5f8b3e320538e621e14cca77182
Agustin Quintana Murta <agustinqm@gmail.com>
2016-07-11 02:18:53 -0300

    add devise gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,10 +2,13 @@ class ArticlesController < ApplicationController
 
   #GET /articles
   def index
+    # Retorna todos los articulos
     @articles = Article.all
   end
+
   #GET /articles/:id
   def show
+    # Encuentra un registro por su ID
     @article = Article.find(params[:id])
   end
 
@@ -13,15 +16,43 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  #post /articles
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  #POST /articles
   def create
-    @article = Article.new(title: params[:article][:title],
-                            body: params[:article][:body])
+    #@article = Article.new(title: params[:article][:title],body: params[:article][:body])
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render :new
     end
-
   end
+
+  #DELETE /articles/:id
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  #UPDATE /articles/:id
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
+
 end
