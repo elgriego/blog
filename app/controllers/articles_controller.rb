2f8c0a7d64c927f0e7c0edfd409a946c04a3d8e4
Agustin Quintana Murta <agustinqm@gmail.com>
2016-08-09 11:38:03 -0300

    some changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+  #un tipo de "callback"
+  #before_action :valita_user, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index]
+  # before_action :set_article, except: [:index, :new, :create]
+  #o sino
+  #before_action :authenticate_user!, except: [:show, :index]
+
 
   #GET /articles
   def index
@@ -10,6 +17,8 @@ class ArticlesController < ApplicationController
   def show
     # Encuentra un registro por su ID
     @article = Article.find(params[:id])
+    @article.update_visits_count
+    @comment = Comment.new
   end
 
   #GET /articles/new
@@ -24,7 +33,7 @@ class ArticlesController < ApplicationController
   #POST /articles
   def create
     #@article = Article.new(title: params[:article][:title],body: params[:article][:body])
-    @article = Article.new(article_params)
+    @article = current_user.articles.new(article_params)
     if @article.save
       redirect_to @article
     else
@@ -50,6 +59,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def validate_user
+    redirect_to new_user_session_path, notice: "Necesitas iniciar sesion"
+  end
 
   def article_params
     params.require(:article).permit(:title,:body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,18 @@ class Article < ApplicationRecord
   # La tabla => articles
   # Campos => article.title() => 'El titulo del Articulo'
   # Escribir metodos
+  belongs_to :user
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { minimum: 20 }
+  before_create :set_visits_count
+
+  def update_visits_count  
+    self.update(visits_count: self.visits_count + 1)
+  end
+
+  private
+
+  def set_visits_count
+      self.visits_count ||= 0
+  end
 end
